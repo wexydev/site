6ada8d7c7564152717a4cdd49be941034f99b285
wexydev <132200770+wexydev@users.noreply.github.com>
2023-04-30 13:57:30 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# site
+# 🔨 | Kurulum
+
+- Projeyi indirin.
+- Dosyayı açın ve `sağ tık + shift` yaparak powershell'i açın.
+- Terminale `npm install` yazın.
+- Son olarak `npm run start` yazarak projeyi başlatın.
+
+![Görünüm](https://media.discordapp.net/attachments/997489625092325486/1075134298979582116/image.png?width=1030&height=559)
+
+## ⭐ | Destek ol
+
+- Projenin gelişiminden memmnunsanız starlamanızı tavsiye ederim.
+
+> coding and designing: github.com/romanwashere | 'Roman#0001
---
+++ src/config.roman.js
@@ -0,0 +1,12 @@
+module.exports = {
+    github: "wexy",
+    instagram: "wexydev",
+    discord: {
+        username: "'wexydev",
+        id: "1087310280565985291",
+        avatarURL: "https://images-ext-2.discordapp.net/external/Mr15fhmP2Qk0O2z-8_YBFtEtpaN9yb7-3wMrQDeTOrs/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1087310280565985291/799f8c334ead3c7ddf889fbc3e251796.png?width=676&height=676"
+    },
+    website: {
+        description: "Hi, my name is Yiğit. I know javascript language and html language. I am a high school student in Turkey!"
+    }
+}
